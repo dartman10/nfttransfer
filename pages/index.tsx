6123689f4da45b4479cb85636664ab392d129f69
Peter Nash <34408740+dartman10@users.noreply.github.com>
2022-10-23 17:41:26 -0400

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import styles from '../styles/Home.module.css'
 import { WalletConnectButton } from '../components/WalletConnectButton'
 import { UserCard } from '../components/UserCard'
 import { UserNft } from '../components/UserNft'
-import { NetworkToggle } from '../components/NetworkToggle'
 
 const Home: NextPage = () => {
   return (
